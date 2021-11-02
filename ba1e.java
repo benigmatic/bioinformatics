2234e5fcefcb795835f7dcb2b75544f2d06bf98e
Alexandra <70664290+benigmatic@users.noreply.github.com>
2021-11-02 13:46:28 -0400

    Add files via upload

---
+++ ba1e.java
@@ -0,0 +1,148 @@
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+public class clumps {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        // Scan the input file input
+        String input = args[0];
+        File file = new File(input);
+
+        Scanner scan = new Scanner(file);
+
+        String genome = scan.next();
+
+        // System.out.println(genome);
+        int k = scan.nextInt();
+        int L = scan.nextInt();
+        int t = scan.nextInt();
+        System.out.println(genome + " " + k + " " + L + " " + t);
+        // Set<String> FrequentPatterns = new Set <String>;
+        String text;
+        Set<String> set = new HashSet<String>();
+        double power = Math.pow(4, k);
+        int pow = (int) power - 1;
+        int[] Clump = new int[pow];
+        int[] FrequencyArray = new int[pow];
+        for (int i = 0; i < pow; i++) {
+            Clump[i] = 0;
+        }
+        for (int i = 0; i < genome.length() - L; i++) {
+            text = genome.substring(i, i + L);
+            FrequencyArray = ComputingFrequencies(text, k);
+            // Printing frequencies:
+            for (int index = 0; index < pow; index++) {
+                // System.out.print("-"+FrequencyArray[index]);
+                if (FrequencyArray[index] >= t) {
+                    Clump[index] = 1;
+                }
+            }
+        }
+        for (int i = 0; i < pow; i++) {
+            if (Clump[i] == 1) {
+                String pattern = NumberToPattern(i, k);
+                set.add(pattern);
+            }
+        }
+        if (set.size() == 0) {
+            set.add("A");
+            set.add("C");
+            set.add("T");
+            set.add("G");
+        }
+
+        System.out.println(set.toString());
+
+    }
+
+    // ans = ClumpFinding(genome, k, L, t);
+
+    /*
+     * static String[] ClumpFinding(String genome, int k, int L, int t) { String[]
+     * ret; ret[0] = return ret; }
+     */
+    public static int[] ComputingFrequencies(String text, int k) {
+        // System.out.println("In freq");
+
+        String pattern;
+        int j;
+        double power = Math.pow(4, k);
+        int pow = (int) power;
+        int[] freq = new int[pow];
+        for (int i = 0; i < pow - 1; i++) {
+            freq[i] = 0;
+        }
+        for (int i = 0; i < (text.length()) - k; i++) {
+
+            // System.out.println(i + " " + k+ " "+ ((text.length() - 1) - k));
+            pattern = text.substring(i, i + k);
+
+            j = PatternToNumber(pattern);
+
+            freq[j] = freq[j] + 1;
+
+        }
+
+        return freq;
+    }
+
+    static int PatternToNumber(String pattern) {
+        String symbol, Prefix;
+        if (pattern.length() == 0) {
+            return 0;
+        }
+        symbol = pattern.substring(pattern.length() - 1);
+        Prefix = pattern.substring(0, pattern.length() - 1);
+        int pre = PatternToNumber(Prefix);
+        int num = SymbolToNumber(symbol);
+        int ret = 4 * pre + num;
+        return ret;
+
+    }
+
+    static int SymbolToNumber(String symbol) {
+        int ret;
+        if (symbol.equals("A")) {
+            ret = 0;
+        } else if (symbol.equals("C")) {
+            ret = 1;
+        } else if (symbol.equals("G")) {
+            ret = 2;
+        } else {
+            ret = 3;
+        }
+        // System.out.println("Symbol to Number: " + symbol + " " + ret);
+        return ret;
+    }
+
+    static String NumberToPattern(int index, int k) {
+        // Base case when the number is just one number (0,1,2,3)
+        if (k == 1) {
+            return NumberToSymbol(index);
+        }
+
+        int prefixIndex = index / 4;
+        int r = index % 4;
+        String symbol = NumberToSymbol(r);
+        String PrefixPattern = NumberToPattern(prefixIndex, k - 1);
+        return PrefixPattern + symbol;
+
+    }
+
+    static String NumberToSymbol(int index) {
+
+        if (index == 0) {
+            return "A";
+        } else if (index == 1) {
+            return "C";
+        } else if (index == 2) {
+            return "G";
+        } else {
+            return "T";
+        }
+
+    }
+
+}
